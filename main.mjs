37c2fad30c20608961e0b30d35fed4a905b0c6cb
DeividMesquita <115595872+DeividMesquita@users.noreply.github.com>
2023-04-27 12:11:08 -0300

    Add files via upload

---
+++ main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funções.mjs";
+
+console.log(mostraIdade( "Deividson" ,21));
+console.log(mostraCidade('Deividson', 'Fortaleza'));
+console.log(mostraHobby('Deividson', 'fazer música'));
